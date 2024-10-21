399ccbf219028bb29190231665b5a79ce25a2ea2
Alex Karpov <keyfour13@gmail.com>
2024-10-21 21:36:20 +0300

    Add an empty CPU class

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-# Makefile for dsPIC33-renode project
+# Makefile for dsPIC33 Renode plugin
 
 # Compiler and flags
 MCS = mcs
@@ -6,10 +6,19 @@ MCS = mcs
 # Directories
 SRC_DIR = src
 BUILD_DIR = build
+RENODE_LIB_DIR = /opt/renode/bin
 
 # Source files
 PROCESSOR_SRC = $(SRC_DIR)/cpu/DsPic33CPU.cs
 PERIPHERAL_SRC = $(SRC_DIR)/peripherals/*.cs
+
+# Libraries to reference
+REFERENCES = \
+	-r:$(RENODE_LIB_DIR)/Emulator.dll \
+	-r:$(RENODE_LIB_DIR)/Renode-peripherals.dll \
+	-r:$(RENODE_LIB_DIR)/ELFSharp.dll \
+	-r:$(RENODE_LIB_DIR)/Migrant.dll \
+	-r:$(RENODE_LIB_DIR)/Nini.dll
 
 # Output
 OUT_DLL = $(BUILD_DIR)/dsPIC33-renode.dll
@@ -20,7 +29,7 @@ all: $(OUT_DLL)
 # Build target
 $(OUT_DLL): $(PROCESSOR_SRC) $(PERIPHERAL_SRC)
 	@mkdir -p $(BUILD_DIR)
-	$(MCS) -target:library -out:$@ $^
+	$(MCS) -target:library -out:$@ $^ $(REFERENCES)
 
 # Clean target
 clean:
@@ -30,4 +39,4 @@ run: $(OUT_DLL)
 	renode scripts/dsPIC33.resc
 
 # Phony targets
-.PHONY: all clean
+.PHONY: all clean run
--- src/cpu/DsPic33CPU.cs
+++ src/cpu/DsPic33CPU.cs
@@ -1,8 +1,116 @@
-// Placeholder for dsPIC33 Processor definition
-namespace Renode.Plugins.DsPic33
+using System;
+using Antmicro.Renode.Core;
+using Antmicro.Renode.Logging;
+using Antmicro.Renode.Time;
+using ELFSharp.ELF;
+using Machine = Antmicro.Renode.Core.Machine;
+
+namespace Antmicro.Renode.Peripherals.CPU
 {
-    public class DsPic33CPU
+    // parts of this class can be left unmodified;
+    // to integrate an external simulator you need to
+    // look for comments in the code below
+    public class DsPic33CPU : BaseCPU, IGPIOReceiver, ITimeSink, IDisposable
     {
-        // Add processor implementation here
+        public DsPic33CPU(string cpuType, IMachine machine, Endianess endianness = Endianess.LittleEndian, CpuBitness bitness = CpuBitness.Bits32)
+            : base(0, cpuType, machine, endianness, bitness)
+        {
+
+        }
+
+        public override void Start()
+        {
+            base.Start();
+
+            // [Here goes an invocation enabling an external simulator (if needed)]
+            // [This can be used to initialize the simulator, but no actual instructions should be executed yet]
+        }
+
+        public override void Reset()
+        {
+            base.Reset();
+
+            instructionsExecutedThisRound = 0;
+            totalExecutedInstructions = 0;
+
+            // [Here goes an invocation resetting the external simulator (if needed)]
+            // [This can be used to revert the internal state of the simulator to the initial form]
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+            // [Here goes an invocation disposing the external simulator (if needed)]
+            // [This can be used to clean all unmanaged resources used to communicate with the simulator]
+        }
+
+        public void OnGPIO(int number, bool value)
+        {
+            this.NoisyLog("IRQ {0}, value {1}", number, value);
+            if(!IsStarted)
+            {
+                return;
+            }
+
+            // [Here goes an invocation triggering an IRQ in the external simulator]
+        }
+
+        public virtual void SetRegisterValue32(int register, uint value)
+        {
+            // [Here goes an invocation setting the register value in the external simulator]
+        }
+
+        public virtual uint GetRegisterValue32(int register)
+        {
+            // [Here goes an invocation reading the register value from the external simulator]
+            return 0;
+        }
+
+        public override ExecutionResult ExecuteInstructions(ulong numberOfInstructionsToExecute, out ulong numberOfExecutedInstructions)
+        {
+            instructionsExecutedThisRound = 0UL;
+
+            try
+            {
+                // [Here comes the invocation of the external simulator for the given amount of instructions]
+                // [This is the place where simulation of acutal instructions is to be executed]
+            }
+            catch(Exception)
+            {
+                this.NoisyLog("CPU exception detected, halting.");
+                InvokeHalted(new HaltArguments(HaltReason.Abort, this));
+                return ExecutionResult.Aborted;
+            }
+            finally
+            {
+                numberOfExecutedInstructions = instructionsExecutedThisRound;
+                totalExecutedInstructions += instructionsExecutedThisRound;
+            }
+
+            return ExecutionResult.Ok;
+        }
+
+        public override string Architecture => "Unknown";
+
+        public override RegisterValue PC
+        {
+            get
+            {
+                return GetRegisterValue32(PCRegisterId);
+            }
+
+            set
+            {
+                SetRegisterValue32(PCRegisterId, value);
+            }
+        }
+
+        public override ulong ExecutedInstructions => totalExecutedInstructions;
+
+        private ulong instructionsExecutedThisRound;
+        private ulong totalExecutedInstructions;
+
+        // [This needs to be mapped to the id of the Program Counter register used by the simulator]
+        private const int PCRegisterId = 0;
     }
 }
